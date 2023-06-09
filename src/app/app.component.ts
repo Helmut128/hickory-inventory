e75d060545827fd1eb5b4afe5b3836ea34c59447
helmut128 <helmutmartinez128@gmail.com>
2023-06-09 14:48:34 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hdr_inventory_20230609';
+}
